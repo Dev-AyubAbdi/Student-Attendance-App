1e1dbd68b15801901ac8f8cc3f3ca0b631af3050
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2025-12-17 14:41:13 +0300

    added new component of attedance lists

--- src/Components/TeacherDashboard.jsx
+++ src/Components/TeacherDashboard.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { IoMdCheckmark } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import { CiTimer } from "react-icons/ci";
+import { AttendanceLists } from "./AttendanceLists";
 
 export const TeacherDashboard = () => {
+  const [present, setPresent] = useState("")
+  const [absent, setAbsent] = useState('')
+  const [late, setLate] = useState('')
   return (
-    <div>TeacherDashboard</div>
-  )
-}
+    <div className="max-w-7xl mx-auto">
+      <div className="flex justify-self-end items-center mt-5">
+        <span className="pr-3 font-semibold">Welcome, Teacher!</span>
+        <img
+          src="./src/assets/teacherImg.jpg"
+          className="w-15 h-15 rounded-full border p-1 border-blue-400"
+          alt=""
+        />
+      </div>
+      <div className="flex gap-10 mt-6">
+        <div className="bg-green-400 w-100 p-5 rounded-lg text-white ">
+          <div className="pl-4 flex items-center">
+             <IoMdCheckmark className="text-blue-500 bg-white rounded-full mr-7" size={50} />
+            <div>
+              <span className="text-2xl">
+              Present Students</span>
+            <p className="text-center text-4xl font-semibold">20</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-red-400 w-100 p-5 rounded-lg text-white ">
+          <div className="pl-4 flex items-center">
+             <IoMdClose className="text-red-500 font-bold bg-white rounded-full mr-7" size={50} />
+            <div>
+              <span className="text-2xl">
+              Absent Students</span>
+            <p className="text-center text-4xl font-semibold">20</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-blue-400 w-100 p-5 rounded-lg text-white ">
+          <div className="pl-4 flex items-center">
+             <CiTimer className="text-blue-500 bg-white rounded-full mr-7" size={50} />
+            <div>
+              <span className="text-2xl">
+              Late Students</span>
+            <p className="text-center text-4xl font-semibold">20</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Attendance Lists */}
+      <AttendanceLists />
+    </div>
+  );
+};
